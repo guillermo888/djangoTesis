b8a6f4aefc651ce5e6ec65f24e910a04d2055678
guillermo malaga <malaga_891@hotmail.com>
2018-07-26 12:01:00 -0400

    creacion de dos archivos para el index

--- antsystem/views.py
+++ antsystem/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("est aes un prueba de la primera aplicacion")
+	#return HttpResponse("est aes un prueba de la primera aplicacion")
+	return render(
+    request,
+    'index.html',
+     context={'num_books':"num_books",'num_instances':"num_instances",'num_instances_available':"num_instances_available",'num_authors':"num_authors"},
+)
