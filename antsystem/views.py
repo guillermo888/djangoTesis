7b74c5288fe1414f3d72bb643c1cd795d38808b8
guillermo malaga <malaga_891@hotmail.com>
2018-07-27 13:39:54 -0400

    otro nuevo cambio en los archivos

--- antsystem/views.py
+++ antsystem/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+# Create your views here.
 def index(request):
-	#return HttpResponse("est aes un prueba de la primera aplicacion")
-	return render(
-    request,
-    'index.html',
-     context={'num_books':"num_books",'num_instances':"num_instances",'num_instances_available':"num_instances_available",'num_authors':"num_authors"},
-)
+	return HttpResponse("Hola esta es un aprueba del httpresponse")
+#return render(request,'index.html',context={'num_books':"num_books",'num_instances':"num_instances",'num_instances_available':"num_instances_available",'num_authors':"num_authors"},)
